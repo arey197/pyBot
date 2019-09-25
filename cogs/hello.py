9907fd85f823d0834992b1441dd5e479f2b81f0c
Aarón Rey <aaronrey@live.com>
2019-09-25 17:17:58 +0200

    added a "clear" command with some error management

--- cogs/hello.py
+++ cogs/hello.py
@@ -8,16 +8,25 @@ class Hello(commands.Cog):
         self.client = client
 
     # Events
-    '''
     @commands.Cog.listener()
-    async def on_ready(self):
-        print('We have logged in as {0.user}'.format(self.client))
-    '''
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send("That command doesn't exist!")
 
     # Commands
     @commands.command()
     async def ping(self, ctx):
         await ctx.send("Pong!")
+
+    @commands.command()
+    async def clear(self, ctx, amount: int):
+        await ctx.channel.purge(limit=amount+1)
+
+    @clear.error
+    @commands.has_permissions(manage_messages=True)
+    async def clear_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Please specify an amount of messages to delete.")
 
 
 def setup(client):
